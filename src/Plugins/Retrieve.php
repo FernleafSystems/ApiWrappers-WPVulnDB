8d0a6061d3d1387e2d6a54af4ae7951066c5eb4f
Paul G <paul@icontrolwp.com>
2020-06-26 17:01:55 +0100

    adds an asset flag to the plugin theme vuln VO so you know what you have after the request

--- src/Plugins/Retrieve.php
+++ src/Plugins/Retrieve.php
@@ -3,6 +3,7 @@
 namespace FernleafSystems\ApiWrappers\WpVulnDb\Plugins;
 
 use FernleafSystems\ApiWrappers\WpVulnDb\Common\PluginThemeRetrieve;
+use FernleafSystems\ApiWrappers\WpVulnDb\Common\PluginThemeVulnVO;
 
 /**
  * Class Retrieve
@@ -11,4 +12,15 @@ use FernleafSystems\ApiWrappers\WpVulnDb\Common\PluginThemeRetrieve;
 class Retrieve extends PluginThemeRetrieve {
 
 	const ENDPOINT_KEY = 'plugins';
+
+	/**
+	 * @inheritDoc
+	 */
+	public function retrieve() {
+		$oVO = parent::retrieve();
+		if ( $oVO instanceof PluginThemeVulnVO ) {
+			$oVO->asset_type = 'plugin';
+		}
+		return $oVO;
+	}
 }
